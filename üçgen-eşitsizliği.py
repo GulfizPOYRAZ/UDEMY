819b7a7d0d832ff95b9a4ebb7cdb11072c689e8f
gülfiz poyraz <163583143+GulfizPOYRAZ@users.noreply.github.com>
2024-04-10 00:27:28 +0300

    Update üçgen-eşitsizliği.py
    özel üçgen koşulları eklendi.

--- üçgen-eşitsizliği.py
+++ üçgen-eşitsizliği.py
@@ -15,8 +15,16 @@ if (abs(a-b)<c<a+b) and (abs(c-b) < a < c+ b)  and (abs(c-a) < b < c+ a):
      print("\t\t {}<{}<{}\n".format(abs(c-b), a, c + b),"doğru")
      print("\t\t {}<{}<{}\n".format(abs(c - a), b, c + a),"doğru")
      print("------eşitsizlikler doğru olduğu için uzunluklar üçgen belirtir.-----")
+     if ((a == b) and (a != c) and (b != c)):
+         print("********üçgenimiz : ikizkenar üçgen  ******\n")
+     elif((a == c) and (a != b) and (c!= b)):
+         print("********üçgenimiz : ikizkenar üçgen  ******\n")
+     elif ((b == c) and (b != a) and (c != a)):
+         print("********üçgenimiz : ikizkenar üçgen  ******\n")
+     elif (a == b == c):
+         print("********üçgenimiz : eşkenar üçgen  ******\n")
+     else:
+         print("********üçgenimiz : çeşitkenar üçgen  ****** \n")
      print("********üçgen çevresini bulalım={}\t".format(a+b+c))
 else:
     print("------eşitsizlikler doğru olmadığı için üçgen belirtmez.çevre hesaplanamaz.-----")
-
-
