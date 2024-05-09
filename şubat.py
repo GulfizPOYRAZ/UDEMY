5a0e6ba2c408ef54e663a8d094fdf0b1d9a3a1f7
gülfiz poyraz <163583143+GulfizPOYRAZ@users.noreply.github.com>
2024-05-09 22:57:17 +0300

    şubat.py

--- şubat.py
+++ şubat.py
@@ -1,1 +1,11 @@
+print("******* Hangi yıl şubat ayı 29 gündür ?*********\n")
+def artık(yıl):
 
+    if (yıl%4==0):
+
+         print("------->  {}".format(yıl),"yılı şubat ayı 29 gündür.")
+    else:
+        print("------->  {}".format(yıl),"yılı şubat ayı 28 gündür.")
+
+yıl =int(input("\t\t\tyıl giriniz = "))
+artık(yıl)
